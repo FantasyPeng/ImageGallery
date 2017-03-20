f42182db89641bdbd373d4ae80237547410925b0
Tenbeans <1556302431@qq.com>
2017-03-20 15:01:09 +0800

    modify some function to be more strong

--- scripts/showPic.js
+++ scripts/showPic.js
@@ -1,8 +1,39 @@
+addLoadEvent(prepareGallery);
+
+function addLoadEvent(fuc){
+	var onload = window.onload;
+	if (typeof window.onload != "function"){
+		window.onload = fuc;
+	} else {
+		window.onload = function() {
+			onload();
+			func();
+		}
+	}
+}
+
+function prepareGallery(){
+	if (!document.getElementById) return false;
+	if (!document.getElementsByTagName) return false;
+	var gallery = document.getElementById("imagegallery");
+	if (!gallery) return false;
+	var links = gallery.getElementsByTagName("a");
+	for (var i = 0; i < links.length; i++) {
+		links[i].onclick = function(){
+			return !showPic(this);
+		}
+	}
+}
+
 function showPic(whichpic) {
+	if (!document.getElementById("placeholder"))  return false;
 	var source = whichpic.getAttribute("href");
 	var placeholder = document.getElementById("placeholder");
-	var text = whichpic.getAttribute("title");
-	var description = document.getElementById("description");
 	placeholder.setAttribute("src",source);
-	description.firstChild.nodeValue = text;
+	var description = document.getElementById("description");
+	if (description) {
+		var text = whichpic.getAttribute("title");
+		description.firstChild.nodeValue = text;
+	}
+	return true;
 }
