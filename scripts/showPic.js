535351ea479affa3594aa856e97c89e7a3110331
Tenbeans <1556302431@qq.com>
2017-03-22 14:15:01 +0800

    use JavaScript to create the placeholder and description

--- scripts/showPic.js
+++ scripts/showPic.js
@@ -1,14 +1,24 @@
 addLoadEvent(prepareGallery);
+addLoadEvent(preparePlaceholder);
 
-function addLoadEvent(fuc){
+function addLoadEvent(func){   //增加事件处理
 	var onload = window.onload;
 	if (typeof window.onload != "function"){
-		window.onload = fuc;
+		window.onload = func;
 	} else {
 		window.onload = function() {
 			onload();
 			func();
 		}
+	}
+}
+
+function insertAfter(newElement,targetElement) {
+	var parent = targetElement.parentNode;
+	if (parent.lastChild == targetElement) {
+		parent.appendChild(newElement);
+	} else {
+		parent.insertBefore(newElement,targetElement.nextSibling);
 	}
 }
 
@@ -37,4 +47,22 @@ function showPic(whichpic) {
 		description.firstChild.nodeValue = text;
 	}
 	return true;
+}
+
+function preparePlaceholder() {
+	if (!document.createElement) return false;
+	if (!document.createTextNode) return false;
+	if (!document.getElementById) return false;
+	if (!document.getElementById("imagegallery")) return false;
+	var placeholder = document.createElement("img");
+	placeholder.setAttribute("id","placeholder");
+	placeholder.setAttribute("src","http://on459yb7r.bkt.clouddn.com/placeholder.gif");
+	placeholder.setAttribute("alt","my image gallery");
+	var description = document.createElement("p");
+	description.setAttribute("id","description");
+	var txt = document.createTextNode("Choose an image");
+	description.appendChild(txt);
+	var gallery = document.getElementById("imagegallery");	
+	insertAfter(placeholder,gallery);
+	insertAfter(description,placeholder);
 }
